8dff3211837c1d7793ebe16dd90fd1d6976166fe
Jeferson Ruiz <jefryruiz@gmail.com>
2023-04-03 19:56:22 -0400

    Modificado JSforce a clase y también el oauth2

--- jsforce.js
+++ jsforce.js
@@ -1,265 +1,76 @@
 import jsforce from 'jsforce';
-import { aureumTest } from './oauth.js';
-import { updateRecordsWithoutId, updateRecordsLocally } from './database.js';
 
-const currentSfEnv = aureumTest;
-/*
-* General Salesforce API methods
-*/
-  const login = () => {
-    return new Promise((resolve, reject) => {
-      const conn = new jsforce.Connection(currentSfEnv.oauth2);
-      conn.login(currentSfEnv.username, currentSfEnv.password, function(err, userInfo) {
-        if (err) { return reject(err); }
-        console.log(conn.accessToken);
-        console.log(conn.instanceUrl);
-        console.log("User ID: " + userInfo.id);
-        console.log("Org ID: " + userInfo.organizationId);
-        resolve(conn);
-      });
-    });
+class JSForce {
+  constructor(oauth2) {
+    this.oauth2 = oauth2;
   }
-  const CRUDRecords = (sObjectName, CRUDOperation, records) => {
-    return new Promise((resolve, reject) => {
-      login().then((conn) => {
-        let results = {};
-        let ids = [];
-        let errors = [];
-        conn.bulk.pollTimeout = 250000; // Bulk timeout can be specified globally on the connection object
-        conn.bulk.load(sObjectName, CRUDOperation, records, function(err, rets) {
-          if (err) { reject(console.error(err)); }
-          for (var i=0; i < rets.length; i++) {
-            let queryObj = {};
-            if (rets[i].success) {
-              queryObj.Id = rets[i].id;
-              ids.push(queryObj);
-            } else {
-              queryObj.error = "#" + (i+1) + " error occurred, message = " + rets[i].errors.join(', ');
-              errors.push(queryObj);
-            }
-          }
-          results = {ids, errors};
-          resolve(results);
+  /*
+  * General Salesforce API methods
+  */
+    login = () => {
+      return new Promise((resolve, reject) => {
+        const conn = new jsforce.Connection(this.oauth2);
+        conn.login(this.oauth2.username, this.oauth2.password, function(err, userInfo) {
+          if (err) { return reject(err); }
+          console.log(conn.accessToken);
+          console.log(conn.instanceUrl);
+          console.log("User ID: " + userInfo.id);
+          console.log("Org ID: " + userInfo.organizationId);
+          resolve(conn);
         });
-      }).catch((err) => {
-        reject(err);
       });
-    });
-  }
-  const getRecordsByIds = (sObjectName, ids, fields) => {
-    return new Promise((resolve, reject) => {
-      login().then((conn) => {
-        conn.sobject(sObjectName).find({$or: ids}, fields).execute((err, records) => {
+    }
+    CRUDRecords = (sObjectName, CRUDOperation, records) => {
+      return new Promise((resolve, reject) => {
+        this.login().then((conn) => {
+          let results = {};
+          let ids = [];
+          let errors = [];
+          conn.bulk.pollTimeout = 250000; // Bulk timeout can be specified globally on the connection object
+          conn.bulk.load(sObjectName, CRUDOperation, records, function(err, rets) {
+            if (err) { reject(console.error(err)); }
+            for (var i=0; i < rets.length; i++) {
+              let queryObj = {};
+              if (rets[i].success) {
+                queryObj.Id = rets[i].id;
+                ids.push(queryObj);
+              } else {
+                queryObj.error = "#" + (i+1) + " error occurred, message = " + rets[i].errors.join(', ');
+                errors.push(queryObj);
+              }
+            }
+            results = {ids, errors};
+            resolve(results);
+          });
+        }).catch((err) => {
+          reject(err);
+        });
+      });
+    }
+    getRecordsByIds = (sObjectName, ids, fields) => {
+      return new Promise((resolve, reject) => {
+        this.login().then((conn) => {
+          conn.sobject(sObjectName).find({$or: ids}, fields).execute((err, records) => {
+            if (err) { reject(console.error(err)); }
+            resolve(records);
+          });
+        }).catch((err) => {
+          reject(err);
+        });
+      });
+    }
+    getRecordsByIdList = (sObjectName, idList, fields) => {
+      return new Promise((resolve, reject) => {
+        this.login().then((conn) => {
+          conn.sobject(sObjectName).find({Id: {$in: idList} }, fields).execute((err, records) => {
           if (err) { reject(console.error(err)); }
           resolve(records);
+          });
+        }).catch((err) => {
+          reject(err);
         });
-      }).catch((err) => {
-        reject(err);
       });
-    });
-  }
-  const getRecordsByIdList = (sObjectName, idList, fields) => {
-    return new Promise((resolve, reject) => {
-		login().then((conn) => {
-		  conn.sobject(sObjectName).find({Id: {$in: idList} }, fields).execute((err, records) => {
-			if (err) { reject(console.error(err)); }
-			resolve(records);
-		  });
-		}).catch((err) => {
-		  reject(err);
-		});
-	  });
-  }
-/*
-* Account CRUD methods
-*/
-  const insertAccounts = (accountsToinsert) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Account', 'insert', accountsToinsert).then((results) => {
-        if (results.ids.length > 0) {
-          let fields = { Id: 1, au_External_ID__c: 1 }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Account', ids, fields).then((records) => {
-              if (records.length > 0) {
-                console.log(records);
-                updateRecordsWithoutId(records, 'accounts', 'au_External_ID__c');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not accounts to look for');
-          }
-        } else if (results.ids.length == 0 && results.errors.length > 0) {
-          reject(results.errors);
-        }
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
-/*
-* Contact CRUD methods
-*/
-  const insertContacts = (contactsToInsert) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Contact', 'insert', contactsToInsert).then((results) => {
-        if (results.ids.length > 0) {
-          let fields = { Id: 1, au_External_ID__c: 1 }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Contact', ids, fields).then((records) => {
-              if (records.length > 0) {
-                updateRecordsWithoutId(records, 'contacts', 'au_External_ID__c');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not contacts to look for');
-          }
-        } else if (results.ids.length == 0 && results.errors.length > 0) {
-          reject(results.errors);
-        }
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
-/*
-* Case CRUD methods
-*/
-  const insertCases = (casesToInsert) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Case', 'insert', casesToInsert).then((results) => {
-        if (results.ids.length > 0) {
-          let fields = { Id: 1, Case_ExternalId__c: 1 }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Case', ids, fields).then((records) => {
-              if (records.length > 0) {
-                updateRecordsWithoutId(records, 'cases', 'Case_ExternalId__c');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not cases to look for');
-          }
-        }
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
-  const updateCases = (casesToUpdate) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Case', 'update', casesToUpdate).then((results) => {
-		console.log(results);
-        if (results.ids.length > 0) {
-          let fields = {
-			Id: 1,
-			LastModifiedDate: 1,
-			OwnerId: 1,
-			Reason: 1,
-			Priority: 1,
-			Product__c: 1,
-			Status: 1,
-			IsStopped: 1,
-			Type: 1,
-			Description: 1
-		  }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Case', ids, fields).then((records) => {
-              if (records.length > 0) {
-                updateRecordsLocally(records, 'cases', 'Id');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not cases to look for');
-          }
-        } else if (results.ids.length == 0 && results.errors.length > 0){
-			reject(results.errors);
-		}
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
-/*
-* Opportunity CRUD methods
-*/
-  const insertOpportunities = (opportunitiesToInsert) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Opportunity', 'insert', opportunitiesToInsert).then((results) => {
-        if (results.ids.length > 0) {
-          let fields = { Id: 1, TrackingNumber__c: 1 }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Opportunity', ids, fields).then((records) => {
-              if (records.length > 0) {
-                updateRecordsWithoutId(records, 'opportunities', 'TrackingNumber__c');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not cases to look for');
-          }
-        }
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
-  const updateOpportunities = (opportunitiesToUpdate) => {
-    return new Promise((resolve, reject) => {
-      CRUDRecords('Opportunity', 'update', opportunitiesToUpdate).then((results) => {
-		console.log(results);
-        if (results.ids.length > 0) {
-          let fields = {
-			Id: 1,
-			LastModifiedDate: 1,
-			Amount: 1,
-			ForecastCategoryName: 1,
-			NextStep: 1,
-			OwnerId: 1,
-			Opportunity_Source__c: 1,
-			Probability: 1,
-			TotalOpportunityQuantity: 1,
-			StageName: 1,
-			Type: 1,
-			Description: 1
-		  }
-          let ids = results.ids.filter(id => id.Id);
-          if (ids.length > 0) {
-            getRecordsByIds('Opportunity', ids, fields).then((records) => {
-              if (records.length > 0) {
-                updateRecordsLocally(records, 'opportunities', 'Id');
-                resolve(results);
-              }
-            }).catch((err) => {
-              reject(err);
-            });
-          } else {
-            reject('There are not opportunities to look for');
-          }
-        } else if (results.ids.length == 0 && results.errors.length > 0){
-			reject(results.errors);
-		}
-      }).catch((err) => {
-        reject(err);
-      });
-    });
-  }
+    }
+}
 
-export { insertAccounts, insertContacts, insertCases, insertOpportunities, updateCases, updateOpportunities, getRecordsByIds, getRecordsByIdList }
+export { JSForce }
--- oauth.js
+++ oauth.js
@@ -1,51 +1,46 @@
-const UnicomerDev1 = {
-	oauth2 : {
-		loginUrl : 'https://test.salesforce.com',
-		clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz8bNZNPIBlRSFltNqWBVYqLxwvhacvgHvbPZPyOTNYUY2Mnm0NOkHxgNSzca',
-		clientSecret : '97101FB63BC3B8C3C19510F15C7775BD5C1FBC72374F0745131C7DFD0F391108',
-		redirectUri : 'http://localhost:8080'
-	},
-	username : 'integracion_sf@unicomer.com.dev1',
-	password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+class OAuht2 {
+  constructor() {
+    this.unicomerDev1 = {
+      oauth2 : {
+        loginUrl : 'https://test.salesforce.com',
+        clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz8bNZNPIBlRSFltNqWBVYqLxwvhacvgHvbPZPyOTNYUY2Mnm0NOkHxgNSzca',
+        clientSecret : '97101FB63BC3B8C3C19510F15C7775BD5C1FBC72374F0745131C7DFD0F391108',
+        redirectUri : 'http://localhost:8080'
+      },
+      username : 'integracion_sf@unicomer.com.dev1',
+      password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+    }
+    this.unicomerTest = {
+      oauth2 : {
+        loginUrl : 'https://test.salesforce.com',
+        clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz_0UEQu6CbttB9d3HHSLfUUJ6LRGlEmFFz_EmgTug8VmF4XEw4JMjNIj.9sj',
+        clientSecret : '91BCA5051FF30C474086A7BDE3E69676FC10BFEC592F83C013CB07FADB76159F',
+        redirectUri : 'http://localhost:8080'
+      },
+      username : 'integracion_sf@unicomer.com.test',
+      password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+    }
+    this.aureumTest = {
+      oauth2 : {
+        loginUrl : 'https://login.salesforce.com',
+        clientId : '3MVG9X12xD2kqQmY0HTAnrgzEMZQ9T2rPZxUM8BVb41jGSNX5.NZ1DOBJLvMwZG5XFn_.fxBCvdJ_ms5UF4aE',
+        clientSecret : '283E69C232108883C2B140CD2B418CF6F1742AEC2BCF149F59C51609B6C756CB',
+        redirectUri : 'http://localhost:8080'
+      },
+      username : 'iuser@aureumtest.us',
+      password : 'aureum123456a2tZPi0GiXyo6RyaRFwobuq1'
+    }
+    this.ziurfreelance = {
+      oauth2 : {
+        loginUrl : 'https://login.salesforce.com',
+        clientId : '3MVG9KsVczVNcM8zldYMMbz.6W8z48ZH765Tno.4QFuezjuX_EFBUbAEJt6V4N.Mdk0XaWfTyAFcmKzKc4PHr',
+        clientSecret : '794B6798C62E766C1C192B8970B81B27F07D79F66D32D0368E3244D7DCEE3E31',
+        redirectUri : 'https://login.salesforce.com/services/oauht2/callback'
+      },
+      username : 'jeferson@ziurfreelance.com',
+      password : '@ZiuR11235pOFAcldC96as8ahzcgN5Y9I8'
+    }
+  }
 }
-const UnicomerTest = {
-	oauth2 : {
-		loginUrl : 'https://test.salesforce.com',
-		clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz_0UEQu6CbttB9d3HHSLfUUJ6LRGlEmFFz_EmgTug8VmF4XEw4JMjNIj.9sj',
-		clientSecret : '91BCA5051FF30C474086A7BDE3E69676FC10BFEC592F83C013CB07FADB76159F',
-		redirectUri : 'http://localhost:8080'
-	},
-	username : 'integracion_sf@unicomer.com.test',
-	password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
-}
-const UnicomerHVSTST = {
-	oauth2 : {
-		loginUrl : 'https://test.salesforce.com',
-		clientId : '3MVG9lJB4lV8F4SjL8hJVTzsX0OCRYsiaWR61VPH8fCHxrrgrgNbbajkNrDVmDpxkp5j1NUDWiUam9.5zNqak',
-		clientSecret : '45FCE016A9A1B3805FB6AB2F68CAD9B84ACF15EF64C4E6B65B99E755902EDD39',
-		redirectUri : 'http://localhost:8080'
-	},
-	username : 'jeferson_ruiz@unicomer.com.hvsdev',
-	password : 'jef123456SYiAOQzeQ0U7ldNZLheEzeSh'
-}
-const aureumTest = {
-	oauth2 : {
-		loginUrl : 'https://login.salesforce.com',
-		clientId : '3MVG9X12xD2kqQmY0HTAnrgzEMZQ9T2rPZxUM8BVb41jGSNX5.NZ1DOBJLvMwZG5XFn_.fxBCvdJ_ms5UF4aE',
-		clientSecret : '283E69C232108883C2B140CD2B418CF6F1742AEC2BCF149F59C51609B6C756CB',
-		redirectUri : 'http://localhost:8080'
-	},
-	username : 'iuser@aureumtest.us',
-	password : 'aureum123456a2tZPi0GiXyo6RyaRFwobuq1'
-}
-const ziurfreelance = {
-	oauth2 : {
-		loginUrl : 'https://login.salesforce.com',
-		clientId : '3MVG9KsVczVNcM8zldYMMbz.6W8z48ZH765Tno.4QFuezjuX_EFBUbAEJt6V4N.Mdk0XaWfTyAFcmKzKc4PHr',
-		clientSecret : '794B6798C62E766C1C192B8970B81B27F07D79F66D32D0368E3244D7DCEE3E31',
-		redirectUri : 'https://login.salesforce.com/services/oauht2/callback'
-	},
-	username : 'jeferson@ziurfreelance.com',
-	password : '@ZiuR11235pOFAcldC96as8ahzcgN5Y9I8'
-}
-export {UnicomerDev1, UnicomerTest, UnicomerHVSTST, aureumTest, ziurfreelance}
+
+export {OAuht2}
