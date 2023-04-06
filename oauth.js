ed16c66746ff2d0fb96c4bf31df144abca4f9a95
Jeferson Ruiz <jefryruiz@gmail.com>
2023-04-06 09:28:59 -0400

    Agregados oauth para las demás orgs

--- oauth.js
+++ oauth.js
@@ -10,6 +10,26 @@ class OAuht2 {
       username : 'iuser@aureumtest.us',
       password : 'aureum123456a2tZPi0GiXyo6RyaRFwobuq1'
     }
+	this.UnicomerDev1 = {
+		oauth2 : {
+			loginUrl : 'https://test.salesforce.com',
+			clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz8bNZNPIBlRSFltNqWBVYqLxwvhacvgHvbPZPyOTNYUY2Mnm0NOkHxgNSzca',
+			clientSecret : '97101FB63BC3B8C3C19510F15C7775BD5C1FBC72374F0745131C7DFD0F391108',
+			redirectUri : 'http://localhost:8080'
+		},
+		username : 'integracion_sf@unicomer.com.dev1',
+		password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+	}
+	this.UnicomerTest = {
+		oauth2 : {
+			loginUrl : 'https://test.salesforce.com',
+			clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz_0UEQu6CbttB9d3HHSLfUUJ6LRGlEmFFz_EmgTug8VmF4XEw4JMjNIj.9sj',
+			clientSecret : '91BCA5051FF30C474086A7BDE3E69676FC10BFEC592F83C013CB07FADB76159F',
+			redirectUri : 'http://localhost:8080'
+		},
+		username : 'integracion_sf@unicomer.com.test',
+		password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+	}
   }
 }
 
