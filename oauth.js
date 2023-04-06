c863e6ec5aeb9a542f4ace0328e533e623b4369d
Jeferson Ruiz <jefryruiz@gmail.com>
2023-04-06 09:30:41 -0400

    Agregados oauth para las demás orgs bien

--- oauth.js
+++ oauth.js
@@ -1,5 +1,25 @@
 class OAuht2 {
   constructor() {
+    this.unicomerDev1 = {
+      oauth2 : {
+        loginUrl : 'https://test.salesforce.com',
+        clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz8bNZNPIBlRSFltNqWBVYqLxwvhacvgHvbPZPyOTNYUY2Mnm0NOkHxgNSzca',
+        clientSecret : '97101FB63BC3B8C3C19510F15C7775BD5C1FBC72374F0745131C7DFD0F391108',
+        redirectUri : 'http://localhost:8080'
+      },
+      username : 'integracion_sf@unicomer.com.dev1',
+      password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+    }
+    this.unicomerTest = {
+      oauth2 : {
+        loginUrl : 'https://test.salesforce.com',
+        clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz_0UEQu6CbttB9d3HHSLfUUJ6LRGlEmFFz_EmgTug8VmF4XEw4JMjNIj.9sj',
+        clientSecret : '91BCA5051FF30C474086A7BDE3E69676FC10BFEC592F83C013CB07FADB76159F',
+        redirectUri : 'http://localhost:8080'
+      },
+      username : 'integracion_sf@unicomer.com.test',
+      password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
+    }
     this.aureumTest = {
       oauth2 : {
         loginUrl : 'https://login.salesforce.com',
@@ -10,26 +30,16 @@ class OAuht2 {
       username : 'iuser@aureumtest.us',
       password : 'aureum123456a2tZPi0GiXyo6RyaRFwobuq1'
     }
-	this.UnicomerDev1 = {
-		oauth2 : {
-			loginUrl : 'https://test.salesforce.com',
-			clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz8bNZNPIBlRSFltNqWBVYqLxwvhacvgHvbPZPyOTNYUY2Mnm0NOkHxgNSzca',
-			clientSecret : '97101FB63BC3B8C3C19510F15C7775BD5C1FBC72374F0745131C7DFD0F391108',
-			redirectUri : 'http://localhost:8080'
-		},
-		username : 'integracion_sf@unicomer.com.dev1',
-		password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
-	}
-	this.UnicomerTest = {
-		oauth2 : {
-			loginUrl : 'https://test.salesforce.com',
-			clientId : '3MVG9eMnfmfDO5NDmXW4W0sFRz_0UEQu6CbttB9d3HHSLfUUJ6LRGlEmFFz_EmgTug8VmF4XEw4JMjNIj.9sj',
-			clientSecret : '91BCA5051FF30C474086A7BDE3E69676FC10BFEC592F83C013CB07FADB76159F',
-			redirectUri : 'http://localhost:8080'
-		},
-		username : 'integracion_sf@unicomer.com.test',
-		password : 'UNIcomer@2022CyWF52cNB5mYVLISoM6hsJnDO'
-	}
+    this.ziurfreelance = {
+      oauth2 : {
+        loginUrl : 'https://login.salesforce.com',
+        clientId : '3MVG9KsVczVNcM8zldYMMbz.6W8z48ZH765Tno.4QFuezjuX_EFBUbAEJt6V4N.Mdk0XaWfTyAFcmKzKc4PHr',
+        clientSecret : '794B6798C62E766C1C192B8970B81B27F07D79F66D32D0368E3244D7DCEE3E31',
+        redirectUri : 'https://login.salesforce.com/services/oauht2/callback'
+      },
+      username : 'jeferson@ziurfreelance.com',
+      password : '@ZiuR11235pOFAcldC96as8ahzcgN5Y9I8'
+    }
   }
 }
 
