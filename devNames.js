c796e8c8cb3c27c6d53bc328da8a01f9783f68b5
Jeferson Ruiz <jefryruiz@gmail.com>
2024-01-10 00:22:12 -0300

    Agregados métodos para poder buscar roles hijos desde rol dado y así encontrar los usuarios

--- devNames.js
+++ devNames.js
@@ -3,10 +3,30 @@ let accountDevName = 'Account'
 let chequeDevName = 'cheque__c'
 let userDevName = 'User'
 let finalcialAccountDevName = 'FinServ__FinancialAccount__c'
+let caseDevName = 'Case'
+let userRoleDevName = 'UserRole'
+/** LISTAS */
+let commercialProfiles = [
+  'Asistente Comercial',
+  'Ejecutivo Comercial',
+  'Ejecutivo Comercial Banca Privada',
+  'Gerente Comercial',
+  'Gerente Regional',
+  'Jefe Comercial'
+]
+/** FILTROS */
+let commercialUsersStandardFilters = [
+  { Title: { $nlike: '%EJECUTIVO INTEGRAL%' } },
+  { 'Profile.Name' : { $in: commercialProfiles } },
+]
 export {
     branchDevName,
     accountDevName,
     chequeDevName,
     userDevName,
-    finalcialAccountDevName
+    finalcialAccountDevName,
+    caseDevName,
+    userRoleDevName,
+    commercialProfiles,
+    commercialUsersStandardFilters
 }
