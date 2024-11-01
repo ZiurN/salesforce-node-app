89bbc1ac6bebb00fef6bd673eb79a5d80a2f06cf
Jeferson Ruiz <jefryruiz@gmail.com>
2024-11-01 10:43:00 -0300

    Removed Project Specified Logic

--- oauth.js
+++ oauth.js
@@ -10,36 +10,6 @@ class OAuth2 {
       username : 'jeferson@ziurfreelance.com',
       password : '@ZiuR11235qa9zUYViTDTOiqWrHjPgv6j8'
     }
-    this.injuryAlliance = {
-      oauth2 : {
-        loginUrl : 'https://injuryalliance.my.salesforce.com/',
-        clientId : '3MVG9KsVczVNcM8zldYMMbz.6W8z48ZH765Tno.4QFuezjuX_EFBUbAEJt6V4N.Mdk0XaWfTyAFcmKzKc4PHr',
-        clientSecret : '794B6798C62E766C1C192B8970B81B27F07D79F66D32D0368E3244D7DCEE3E31',
-        redirectUri : 'https://login.salesforce.com/services/oauth2/success'
-      },
-      username : 'jruiz@sfsmart.ai',
-      password : '@ZiuR11235qV23iyywxUl9dGOblwt1o4wO1'
-    }
-    this.patDevSBX = {
-      oauth2 : {
-        loginUrl : 'https://aba--patdevsbx.sandbox.my.salesforce.com/',
-        clientId : '3MVG9srSPoex34FUsebMFmjfzB3wTMoZlmhA2SzSWKHC4SDn1gO1Mwx1AujYXetxAU6cA4MvBwakZXgdsWi9d',
-        clientSecret : '9CCAE870375559A7D509678934F0BA0F13F9E58651D7BF58709F373AAB6445B3',
-        redirectUri : 'https://login.salesforce.com/services/oauth2/success'
-      },
-      username : 'avram@ablairlaw.com.patdevsbx',
-      password : 'SiyaRam@69QFIqjEvD2AyrXRFFnBPVgfOJg'
-    }
-    this.ABAProd = {
-      oauth2 : {
-        loginUrl : 'https://avramblairandassociates.my.salesforce.com/',
-        clientId : '3MVG9xOCXq4ID1uEfFlBe76plruVGl7EjKOIlpPzWhnLHWilHIF_1sFFDuFeOZfl53F7K04JRTjIDk.L9mM9f',
-        clientSecret : '7E5A9D3E800A242483EF12A7683C0E3F9DF3BDCD34BA6F1D6317B10E0A4C000D',
-        redirectUri : 'https://login.salesforce.com/services/oauth2/success'
-      },
-      username : 'autumn@ablairlaw.com',
-      password : 'Suzanne201883Ralpc2ibiFZ1oqgX8nGBzv'
-    }
   }
 }
 
